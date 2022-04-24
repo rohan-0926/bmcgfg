38a16f4c04458ecd4eeb76908501946dd7d2357c
RohanKondam <84554858+rohan-0926@users.noreply.github.com>
2022-04-24 20:52:42 +0530

    Update minbitflipstoconvert.cpp

--- minbitflipstoconvert.cpp
+++ minbitflipstoconvert.cpp
@@ -16,17 +16,7 @@ typedef pair<int,int> pii;
 const ll mod = 1e9+7, N = 2e6+7, M = 2e6+7, INF = INT_MAX/10;
 int dirx[]={-1,-1,0,1,1,1,0,-1};
 int diry[]={0,1,1,1,0,-1,-1,-1};
-int diagele(int i,int j,vector<vector<int>>&v)
-{
-    int ele=0;
-    while(i<v.size() && j<v[0].size())
-    {
-       ele=v[i][j];
-        i++;
-        j++;
-    }
-    return ele;
-}
+
 void solve()
 {
    int start,goal;cin>>start>>goal;
